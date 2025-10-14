bd840e1efac7234e7ca5f5b117c3efc86aebec95
Supermango407 <150284897+Supermango407@users.noreply.github.com>
2025-10-14 16:05:56 -0500

    started logic chain

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import setttings
+
+root = tk.Tk()
+root.title("Logic Chain")
+root.geometry("800x800+1640+40")
+root.minsize(400, 400)
+root.state('zoomed')
+root.config(bg=setttings.background_color)
+
+root.mainloop()
