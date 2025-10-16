f6b8960e166a00a40ddbbab3f2a17033ba00fc16
Supermango407 <150284897+Supermango407@users.noreply.github.com>
2025-10-15 21:14:20 -0500

    created main frames

--- main.py
+++ main.py
@@ -1,23 +1,100 @@
 import tkinter as tk
-import setttings
+from tkinter.font import Font
+import screeninfo
+import settings
 import dbhandler
+
 
 root = tk.Tk()
 root.title("Logic Chain")
-root.geometry("800x800+1640+40")
+
+# set screen size
+if len(screeninfo.get_monitors()) == 2: # two monitors
+    root.geometry(f"800x800+{root.winfo_screenwidth()+250}+40")
+else:
+    root.geometry(f"800x800+250+40")
+
 root.minsize(400, 400)
 root.state('zoomed')
-root.config(bg=setttings.background_color)
+root.config(bg='black')
 
 dbhandler.load()
-print("\nstatments:")
-for i in dbhandler.statments:
-    statment = dbhandler.statments[i]
-    print(f"  {i}:\t{statment}")
+# print("\nstatments:")
+# for i in dbhandler.statments:
+#     statment = dbhandler.statments[i]
+#     print(f"  {i}:\t{statment}")
 
-print("\nopinions:")
-for i in dbhandler.opinions:
-    opinion = dbhandler.opinions[i]
-    print(f"  {i}:\t{opinion.text}")
+# print("\nopinions:")
+# for i in dbhandler.opinions:
+    # opinion = dbhandler.opinions[i]
+    # print(f"  {i}:\t{opinion.text}")
+
+
+class Header(object):
+
+    def __init__(self):
+        self.frame = tk.Frame(root, background=settings.header_color, padx=4, pady=4)
+        self.frame.pack(side='top', fill='x')
+
+        self.menu_button = tk.Button(self.frame, text="Menu", font=main_font, command=menu.toggle)
+        self.menu_button.pack(side='left')
+
+        self.header_text = tk.Label(self.frame, text='Opinion', font=header_font, background=settings.header_color)
+        self.header_text.pack(side='top', expand=True)
+
+
+class Menu(object):
+
+    def __init__(self):
+        self.frame = tk.Frame(root, background=settings.menu_color)
+        # self.frame.pack(side='left', fill='y')
+
+        self.text = tk.Label(self.frame, text="Opinions", padx=4, pady=4, fg='white', background=settings.menu_color, font=main_font)
+        self.text.pack(side='top')
+
+    def toggle(self):
+        if self.frame.winfo_ismapped():
+            self.close()
+        else:
+            self.open()
+
+    def open(self):
+        self.frame.pack(side='left', fill='y', after=header.frame)
+
+    def close(self):
+        self.frame.pack_forget()
+
+
+class Window():
+
+    def __init__(self):
+        self.frame = tk.Frame(root, background=settings.border_color)
+        self.frame.pack(side='top', fill='both', expand=True)
+        self.frame.grid_rowconfigure(0, weight=1)
+        self.frame.grid_columnconfigure(0, weight=1)
+
+        self.pros_frame = tk.Frame(self.frame, background=settings.background_color, padx=1, pady=1)
+        self.cons_frame = tk.Frame(self.frame, background=settings.background_color, padx=1, pady=1)
+
+        self.pros_frame.pack(side='left', fill='both', expand=True, padx=1, pady=1)
+        self.cons_frame.pack(side='right', fill='both', expand=True, padx=1, pady=1)
+    
+    def open_table(self, opinion:dbhandler.Opinion):
+        """opens pros/cons table of `opinion`."""
+        self.pros_frame = tk.Frame(self.frame, background='blue', borderwidth=2)
+        self.cons_frame = tk.Frame(self.frame, background='red', borderwidth=2)
+
+        self.pros_frame.place(anchor='w', height=1, width=0.4)
+        self.cons_frame.place(anchor='e', height=1, width=0.4)
+
+
+main_font = Font(family="Consolas", size=int(settings.font_size), weight="normal")
+header_font = Font(family="Consolas", size=int(settings.font_size*1.5), weight="normal")
+
+menu = Menu()
+header = Header()
+window = Window()
+
+# window.open_table(dbhandler.opinions[1])
 
 root.mainloop()
---
+++ settings.py
@@ -0,0 +1,6 @@
+background_color = 'gray10'
+header_color = 'steelblue4'
+menu_color = 'gray5'
+border_color = 'gray40'
+
+font_size = 24
